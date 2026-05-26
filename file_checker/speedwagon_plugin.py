2e86d5170fcfe2327b1fcb2da6be55c831feadaf
hborcher <hborcher@illinois.edu>
2026-05-26 09:18:33 -0500

    update settings widgets

--- file_checker/speedwagon_plugin.py
+++ file_checker/speedwagon_plugin.py
@@ -1,49 +1,147 @@
-from typing import List, Mapping, Sequence, Any
+import os.path
+from typing import List, Mapping, Sequence, Any, Optional, Dict
 
 import speedwagon
 from speedwagon.job import _T
 from speedwagon import workflow
 from speedwagon.tasks import Result, TaskBuilder
+from speedwagon.validators import ReportT
+from speedwagon.workflow import UserDataType
+from datetime import date
 from . import file_checker
+
 
 @speedwagon.hookimpl
 def registered_workflows():
-    return {"File Checker" : FileCheckerWorkflow}
+    return {"File Checker": FileCheckerWorkflow}
+
+
+class ContainsRequiredFolders(speedwagon.validators.AbsOutputValidation):
+    name = "Contains Required Folders"
+
+    def investigate(
+        self, candidate: Optional[_T], job_options: Dict[str, UserDataType]
+    ) -> List[ReportT]:
+        return ["this is an error"]
+
+
+def contains_required_folders(value, expected_folder) -> bool:
+    if not os.path.exists(os.path.join(value, expected_folder)):
+        return False
+    return True
+
+
+def _get_default_report_location() -> str:
+    day = str(date.today())
+    report_title = f"{day}_file_checker_report.txt"
+    desktop_path = os.path.join(os.path.join(os.path.expanduser("~")), "Desktop")
+    return os.path.join(desktop_path, report_title)
+
+
+def is_valid_location_for_log(value) -> bool:
+    dir_path = os.path.dirname(value)
+    return os.path.exists(dir_path)
 
 
 class FileCheckerWorkflow(speedwagon.Workflow):
     name = "File Checker"
+    description = """This program checks that files are consistent with DS conventions for archival and cataloged collections:
+    - All files are either .tifs or .jp2s
+    - File names do not contain any spaces
+    - File names contain a hyphen
+    - File names contain the appropriate level of padding (3 or 8 digits)
+    - File names are sequential
+    - All files that are in the access directory are also present in the corresponding preservation directory,
+      and vice versa.
+      
+Settings:
+
+    Input Directory: Path to validate.
+    File Type: Either archival or cataloged
+    Report File: Location to save report. By default, this will be saved to the desktop with a name in the format YYYY-MM-DD_file_checker_report.txt
+"""
 
     def job_options(self):
+        input_directory = speedwagon.workflow.DirectorySelect(
+            "Input Directory", required=True
+        )
+        input_directory.add_validation(
+            speedwagon.validators.CustomValidation(
+                query=lambda value, _: os.path.exists(value) and os.path.isdir(value),
+                failure_message_function=lambda *_: "Not a valid directory.",
+            )
+        )
+
+        input_directory.add_validation(
+            speedwagon.validators.CustomValidation(
+                query=lambda value, _: contains_required_folders(
+                    value, expected_folder="access"
+                ),
+                failure_message_function=lambda *_: (
+                    "Does not contain required access folder."
+                ),
+            ),
+            condition=lambda candidate, _: os.path.exists(candidate),
+        )
+
+        input_directory.add_validation(
+            speedwagon.validators.CustomValidation(
+                query=lambda value, _: contains_required_folders(
+                    value, expected_folder="preservation"
+                ),
+                failure_message_function=lambda *_: (
+                    "Does not contain required preservation folder."
+                ),
+            ),
+            condition=lambda candidate, _: os.path.exists(candidate),
+        )
+
         file_type = workflow.ChoiceSelection("File Type", required=True)
         file_type.add_selection("Archival")
         file_type.add_selection("Cataloged")
-        return [
-            speedwagon.workflow.DirectorySelect("Input Directory", required=True),
-            file_type
-        ]
+        report_file = workflow.TextLineEditData("Report File", required=True)
+        report_file.value = _get_default_report_location()
+        report_file.add_validation(
+            speedwagon.validators.CustomValidation(
+                query=lambda value, _: is_valid_location_for_log(value),
+                failure_message_function=lambda value: (
+                    f"invalid file location {os.path.dirname(value)}"
+                ),
+            )
+        )
+        return [input_directory, file_type, report_file]
 
     def discover_task_metadata(
-            self,
-            initial_results: List[Result],
-            additional_data: Mapping[str, Any],
-            user_args: _T,
+        self,
+        initial_results: List[Result],
+        additional_data: Mapping[str, Any],
+        user_args: _T,
     ) -> Sequence[Mapping[str, object]]:
-        access_directory, directories, pres_directory, report_location = file_checker.get_directories(user_args["Input Directory"])
-        print(user_args)
+        access_directory, directories, pres_directory, _ = file_checker.get_directories(
+            user_args["Input Directory"]
+        )
         return [
             {
                 "access_directory": access_directory,
                 "directories": directories,
                 "pres_directory": pres_directory,
-                "report_location": report_location,
-                "file_type": user_args["File Type"]
+                "report_location": user_args["Report File"],
+                "file_type": user_args["File Type"],
             }
         ]
 
+    def generate_report(cls, results: List[Result], user_args: _T) -> Optional[str]:
+        total_errors = len(results[0].data["errors"])
+        report_location = user_args["Report File"]
+
+        return (
+            f"{total_errors} discrepancies were discovered. "
+            f"Detailed report saved to {report_location}"
+        )
+
     def create_new_task(self, task_builder: TaskBuilder, job_args) -> None:
         task_builder.add_subtask(
-            FileCheckerTask(
+            check_files(
                 access_directory=job_args["access_directory"],
                 pres_directory=job_args["pres_directory"],
                 directories=job_args["directories"],
@@ -52,37 +150,34 @@ class FileCheckerWorkflow(speedwagon.Workflow):
             )
         )
 
-class FileCheckerTask(speedwagon.tasks.Subtask[None]):
-    name = "File Checker Task"
 
-    def __init__(self, access_directory: str, directories: List[str], pres_directory: str, report_location: str, file_type: str):
-        super().__init__()
-        self.access_directory = access_directory
-        self.directories = directories
-        self.pres_directory = pres_directory
-        self.report_location = report_location
-        self.file_type = file_type
+@speedwagon.tasks.workflow_task(description="Checking files")
+def check_files(
+    access_directory: str,
+    directories: List[str],
+    pres_directory: str,
+    report_location: str,
+    file_type: str,
+) -> str:
+    with open(report_location, "a") as outfile:
+        file_checker.make_report_header(directories, outfile)
 
-    def task_description(self) -> str:
-        return f"Checking files in {self.access_directory} and {self.pres_directory}"
+        for directory in directories:
+            outfile.write(f"\nREPORT FOR {directory}")
+            error_list = file_checker.check_filenames(directory, file_type)
+            for error in error_list:
+                outfile.write(f"\n{error}")
 
-    def work(self) -> bool:
-        with open(self.report_location, 'a') as outfile:
-            file_checker.make_report_header(self.directories, outfile)
+        access_list_trimmed, pres_list_trimmed = file_checker.make_file_number_lists(
+            access_directory, pres_directory
+        )
+        matching_error_list = file_checker.validate_files(
+            access_list_trimmed, pres_list_trimmed
+        )
 
-            for directory in self.directories:
-                outfile.write(f'\nREPORT FOR {directory}')
-                error_list = file_checker.check_filenames(directory, self.file_type)
-                for error in error_list:
-                    outfile.write(f'\n{error}')
-
-            access_list_trimmed, pres_list_trimmed = file_checker.make_file_number_lists(self.access_directory, self.pres_directory)
-            matching_error_list = file_checker.validate_files(access_list_trimmed, pres_list_trimmed)
-
-            for error in matching_error_list:
-                outfile.write(f'\n{error}')
-
-            total_errors = len(error_list) + len(matching_error_list)
-
-            self.log(f'{total_errors} discrepancies were discovered. Detailed report saved to {self.report_location}')
-        return True
+        for error in matching_error_list:
+            outfile.write(f"\n{error}")
+        return {
+            "errors": error_list + matching_error_list,
+            "report location": report_location,
+        }
